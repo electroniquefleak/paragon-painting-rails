82ab34477dc3160df363706aabb28cc1395dd425
Hunter <fleakhunter@gmail.com>
2021-05-10 11:24:16 -0400

    Refactor code.

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,9 +1,6 @@
 class DashboardsController < ApplicationController
-    #before_action to check if the current_user is there (meaning they are logged in).
-    #before_action to get the current_users info
     before_action :require_login
 
-    #dashboard view same as projects/tools indexes (for the specifiic employee)
     def dashboard
         if current_user.admin?
             @projects = Project.all
